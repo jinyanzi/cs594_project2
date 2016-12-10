3cb04ad66ff0cf7dbd151aa57ef3847bdd64205f
yanzi <jinyanzi1990@gmail.com>
2016-12-10 07:43:18 -0600

    stochastic bash updated for resnet

--- code/wide-residual-network/stochastic_train.sh
+++ code/wide-residual-network/stochastic_train.sh
@@ -5,17 +5,19 @@ data='./datasets/cifar10_whitened.t7'
 #data='./datasets/cifar100_whitened.t7'
 
 # resnet with different depth plus stochastic dropout
-#res_depth=(11 47 164 227)
-#for d in "${res_depth[@]}";do
-#	echo $d
-#	model=resnet-pre-act depth=$d stoDrop=0.3 dataset=$data ./scripts/train_cifar.sh
-#done
+res_depth=(11 47 164 227)
+for d in "${res_depth[@]}";do
+	for k in "${dropout[@]}";do
+		echo $d $k
+		model=resnet-pre-act depth=$d stoDrop=$k nGPU=4 dataset=$data ./scripts/train_cifar.sh
+	done
+done
 
 # wide residual network with different widening factor with stochastic dropout
 width=(1 4 8 10)
 for w in "${width[@]}";do
 	for k in "${dropout[@]}";do
 		echo $w $k
-		model=wide-resnet widen_factor=$w depth=16 stoDrop=$k dataset=$data ./scripts/train_cifar.sh
+		model=wide-resnet widen_factor=$w depth=16 stoDrop=$k nGPU=4 dataset=$data ./scripts/train_cifar.sh
 	done
 done
