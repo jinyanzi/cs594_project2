34f3c35947717a2dfa0698dcf72b3f98d4c6ab63
yanzi <jinyanzi1990@gmail.com>
2016-12-10 07:38:38 -0600

    resnet stochastic dropout finished

--- code/wide-residual-network/models/residualBlock.lua
+++ code/wide-residual-network/models/residualBlock.lua
@@ -12,12 +12,12 @@ function ResidualBlock:__init(nInputPlane, nOutputPlane, stride, p)
 	Parent.__init(self)
 	self.p = p or 0
 	self.train = true
-	if torch.ByteTensor(1,1):bernoulli(1-p) > 0 then
+	if torch.ByteTensor(1,1):bernoulli(1-p)[1][1] > 0 then
 		self.gate = true
 	else
 		self.gate = false
 	end
-	print(p, gate)
+	print(p, self.gate)
 	-- The new Residual Unit in [a]
 	local nBottleneckPlane = nOutputPlane / 4
 	-- if opt.resnet_nobottleneck then
