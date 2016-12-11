cb0280bb22a910b883ae6cbbbae791960c975adc
yanzi <jinyanzi1990@gmail.com>
2016-12-10 22:16:26 -0600

    d22w1drop0.5 logs added

--- code/wide-residual-network/stochastic_train.sh
+++ code/wide-residual-network/stochastic_train.sh
@@ -18,21 +18,21 @@ width=(1 4 8 10)
 for w in "${width[@]}";do
 	for k in "${dropout[@]}";do
 		echo $w $k
-		model=wide-resnet widen_factor=$w depth=16 stoDrop=$k nGPU=4 dataset=$data ./scripts/train_cifar.sh
+		model=wide-resnet widen_factor=$w depth=40 stoDrop=$k dataset=$data ./scripts/train_cifar.sh
 	done
 done
 
-# wide residual network with different widening factor with regular dropout
-width=(1 2 4)
-deepen=(2 3 4)
-for w in "${width[@]}";do
-	for d in "${deepen[@]}";do
-		echo $d $w
-		model=wide-resnet widen_factor=$w deepen_factor=$d depth=40 nGPU=4 dataset=$data ./scripts/train_cifar.sh
-	done
-done
+# wide residual network with different deepen factor
+#width=(1 2 4)
+#deepen=(2 3 4)
+#for w in "${width[@]}";do
+#	for d in "${deepen[@]}";do
+#		echo $d $w
+#		model=wide-resnet widen_factor=$w deepen_factor=$d depth=40 nGPU=4 dataset=$data ./scripts/train_cifar.sh
+#	done
+#done
 
-# wide residual network with different widening factor with stochastic dropout
+# wide residual network with different GPU number
 #width=(1 4 8 10)
 #for w in "${width[@]}";do
 #	for k in "${dropout[@]}";do
